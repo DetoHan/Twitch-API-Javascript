e61b7da7d748af0e9c5b397765ad2ce19371c70b
Deto <berta.attila.21@gmail.com>
2022-09-22 20:39:55 +0200

    Update twitch-api.js
    Add Options Constant

--- twitch-api.js
+++ twitch-api.js
@@ -1,4 +1,11 @@
-function startCommercial(broadcaster_id, length, options) {
+const options = {
+    Authorization: "Bearer h367fe3lvqhs5xkub8e7pt69rtftnr",
+    'client-id': 'gp762nuuoqcoxypju8c569th9wz7q5',
+    'accept': 'application/vnd.twitchtv.v5+json'
+}
+
+function startCommercial(broadcaster_id, length) {
+    console.log('Hello')
     switch (length) {
         case 30,60,90,120,150,180:
             fetch(`https://api.twitch.tv/helix/channels/commercial`, {
